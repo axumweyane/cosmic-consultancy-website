85238550e99630a865a9746413c57d72769abc85
axumweyane <axumweyane@gmail.com>
2026-06-29 18:35:44 -0400

    Remove helper/scaffolding files (keep CONTENT-TO-FILL.md)

--- start-site.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Opens the Cosmic Consultancy site directly in your default browser.
-rem No server, no Python, no Node required - works on any Windows PC.
-cd /d "%~dp0"
-start "" "%~dp0index.html"
